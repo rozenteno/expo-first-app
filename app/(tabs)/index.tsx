1228508e2bc3655e64d49a0ff52144323bfe42a4
Rodrigo Zenteno <rodrigo.zenteno7@gmail.com>
2025-05-06 18:43:32 -0600

    Added expo-image-picker and handling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,10 +3,31 @@ import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 import { StyleSheet, View } from "react-native";
 
+import { useState } from "react";
+
+import * as ImagePicker from "expo-image-picker";
+
 // Importing local assets
 const PlaceholderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1 /* .1 low quality */,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
   return (
     <View
       style={styles.container}
@@ -17,10 +38,14 @@ export default function Index() {
       // }}
     >
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={selectedImage || PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
       {/* <Text style={styles.text}>Hello Expo.</Text> */}
